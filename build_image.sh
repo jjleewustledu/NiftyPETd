6390734616ea665ecb0c9f96f3dd790d0394d8f4
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2019-01-14 21:01:29 -0600

    jupyter Reconstruction tests passing

--- build_image.sh
+++ build_image.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-nvidia-docker build -t niftypetd-image -f ${HOME2}/Docker/NiftyPETd/Dockerfile ${HOME2}/Docker/NiftyPETd
+nvidia-docker build -t niftypetd-image -f ${HOME_DOCKER}/NiftyPETd/Dockerfile ${HOME_DOCKER}/NiftyPETd
--- run_container.sh
+++ run_container.sh
@@ -3,14 +3,16 @@ CONTAINER_NAME=$1
 IMAGE_NAME=$2
 
 if [[ $# -eq 0 ]] ; then
-    echo 'ERROR: No argument passed for container name.'
+    echo 'USAGE: docker rm some-container'
+    echo 'USAGE: run_container.sh some-container [some-image]'
+    echo 'USAGE: # default some-image := basename some-container -container'
     exit 0
 fi
 if [[ $# -eq 1 ]] ; then
-    echo 'ERROR: No argument passed for image name.'
+    IMAGE_NAME=`basename $1 -container`
     exit 0
 fi
 
-CONTAINER="nvidia-docker run -it --name $CONTAINER_NAME --net=host -v ${HOME2}/Docker/NiftyPETd/:/ds $IMAGE_NAME"
+CONTAINER="nvidia-docker run -it --name $CONTAINER_NAME --net=host -v ${HOME_DOCKER}/NiftyPETd/:/ds $IMAGE_NAME"
 echo 'Starting container with commmand: '$CONTAINER
 eval $CONTAINER
